3a1eab27f2405afeb0597626ee9db259681bf56b
Ruben.dev <ruben.urena.dev@gmail.com>
2025-12-18 21:32:39 -0400

    Finish EventBus and EventManager class

--- Practice/Challenge/OOP/Inner Classes Challenge_Event Manager/src/io/github/rubenurena/manager/services/EventManager.java
+++ Practice/Challenge/OOP/Inner Classes Challenge_Event Manager/src/io/github/rubenurena/manager/services/EventManager.java
@@ -1,34 +1,75 @@
 package io.github.rubenurena.manager.services;
 import io.github.rubenurena.manager.events.core.EventType;
-import io.github.rubenurena.manager.services.Listener.AreaIsFullListener;
 import io.github.rubenurena.manager.services.Listener.Listenable;
-import io.github.rubenurena.manager.services.Listener.MiniDepartmentIsFullListener;
+import io.github.rubenurena.manager.events.core.*;
+
 
 import java.util.*;
 
+
+    /**
+    * This class manages publishers and is the only entry point for creating
+    * and removing subscriptions, which are implemented as a private inner class.
+    */
 public class EventManager {
-    
+    private final EventBus eventBus;
+
+    public EventManager(){
+        eventBus = new EventBus();
+    }
+
+
+    public void publish(Event event ){
+        eventBus.dispatch(event);
+    }
+
+
+    public void createSubscription(EventType eventType, Listenable listener){
+        this.eventBus.new Subscription(eventType,listener);
+    }
+    private void removeSub(EventType _eventType, Listenable _listener){
+        eventBus.getSubscriptions().removeIf((a)-> a.eventType.equals(_eventType)
+                && a.listener.equals(_listener));
+    }
+
+
+
+    /**
+     * Sends events to all registered subscribers.
+     * This class does not decide when subscribers are added or removed.
+     */
     private static class EventBus{
+
         private final Set<Subscription> subscriptions;
+
 
         private EventBus(){
             subscriptions = new HashSet<>();
         }
 
-        private void removeSub(Subscription subscription){
-            subscriptions.remove(subscription);
+        public Set<Subscription> getSubscriptions() {
+            return subscriptions;
         }
 
-        private void removeSub(EventType _eventType, Listenable _listener){
-            for (var s : subscriptions){
-                if (s.eventType.equals(_eventType) && s.listener.equals(_listener)){
-                    removeSub(s);
+
+        private void dispatch(Event event){
+            var list = routing(event);
+            list.forEach( (a)->a.listener.apply());
+        }
+        private ArrayList<Subscription> routing(Event event){
+            ArrayList<Subscription> arrayList = new ArrayList<>();
+
+            subscriptions.forEach( (s)-> {
+                if(s.eventType.equals(event.getEventType())){
+                    arrayList.add(s);
                 }
-            }
+            });
+            return arrayList;
         }
 
 
-
+        // Subscribes a listener to an event type
+        //do not store multiple subscription
         private class Subscription{
             private final EventType eventType;
             private final Listenable listener;
@@ -39,21 +80,24 @@ public class EventManager {
                 subscriptions.add(this);
             }
 
-            public EventType getEventType() {
+            private EventType getEventType() {
                 return eventType;
             }
 
-            public Listenable getListener() {
+            private Listenable getListener() {
                 return listener;
             }
 
+            //Override equals method to compare Listener variable.
             @Override
             public boolean equals(Object obj) {
+                if (this.hashCode() != obj.hashCode()) return false;
                 if (this == obj) return true;
                 if (!(obj instanceof Subscription s)) return false;
                 return this.listener.equals(s.listener);
             }
 
+            //Override hashCode method to compare eventType variable.
             @Override
             public int hashCode() {
                 return eventType.hashCode();
@@ -67,8 +111,6 @@ public class EventManager {
                         '}';
             }
         }
-
-
     }
 
 
